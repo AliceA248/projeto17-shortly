d5c47f9ae39d0bbe8ddc796846c7fe76c7361000
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2023-05-22 12:36:29 -0300

    ajustes schema-url

--- src/schemas/url.schema.js
+++ src/schemas/url.schema.js
@@ -1,7 +1,9 @@
 import joi from "joi";
 
 const urlSchema = joi.object({
-  url: joi.string().uri().required()
+  url: joi
+    .string()
+    .uri({ scheme: ["http", "https"] })
+    .required(),
 });
-
 export default urlSchema;
